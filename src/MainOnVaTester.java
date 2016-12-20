dc842e89942194e194fad99aca8685145cc037c4
Flo <Flo@10.0.2.15>
2016-12-20 10:39:34 +0100

    2.0

--- src/MainOnVaTester.java
+++ src/MainOnVaTester.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
 
 public class MainOnVaTester {
 
 	public static void main(String[] args) {
-		// 404 Not Found Yet
+		github();
 		System.out.println("Wesh !!");
 
 
 		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici. FcS MDR");
-
-
 	}
+		
+	
+	
+	
+	
+	public static void github(){
+	
+		Scanner sc = new Scanner(System.in);
+		String k =	sc.nextLine();
+		
+		}
+		
+	
 }
  

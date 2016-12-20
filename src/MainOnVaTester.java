e3506eb65dd9e20993fd1c29b390adad6c0c20f1
Flo <Flo@10.0.2.15>
2016-12-20 09:43:19 +0100

    ajout des trucs

--- src/MainOnVaTester.java
+++ src/MainOnVaTester.java
@@ -4,6 +4,7 @@ public class MainOnVaTester {
 	public static void main(String[] args) {
 		// 404 Not Found Yet
 		System.out.println("Wesh !!");
+		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici ");
 	}
 
 }

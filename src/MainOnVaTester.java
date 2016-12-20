cabc9923e7d0b57cccfd6dfabe7d73b26ddaf607
christo <christo@10.0.2.15>
2016-12-20 10:31:22 +0100

    yooo

--- src/MainOnVaTester.java
+++ src/MainOnVaTester.java
@@ -6,7 +6,7 @@ public class MainOnVaTester {
 		System.out.println("Wesh !!");
 
 
-		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici. FcS MDR");
+		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici. FcS MDR xptdr");
 
 
 	}

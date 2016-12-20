2d6ac4421649eceea3710db49f8b7478ef3e19f9
Flo <Flo@10.0.2.15>
2016-12-20 10:20:52 +0100

    ghj

--- src/MainOnVaTester.java
+++ src/MainOnVaTester.java
@@ -4,7 +4,7 @@ public class MainOnVaTester {
 	public static void main(String[] args) {
 		// 404 Not Found Yet
 		System.out.println("Wesh !!");
-		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici. FcS");
+		System.out.println("hey mc Fly je croyais t'avoir dit deplus re foutre les pieds ici. FcS hlitg");
 	}
 }
  
